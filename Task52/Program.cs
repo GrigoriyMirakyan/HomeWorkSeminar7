67890dc8fd86172542844b7c40be249633d5a48e
grigoriyMirakyan <grigoriy66660@gmail.com>
2022-06-05 19:08:48 +0300

    Task52 complete

---
+++ Task52/Program.cs
@@ -0,0 +1,81 @@
+﻿/*Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int NumberInput(string text)//Метод ввода и проверки на число
+{
+    bool isInputInt = true;
+    int number = 0;
+    while (isInputInt)
+    {
+        Console.Write(text);
+        string numberSTR = Console.ReadLine();
+        if (int.TryParse(numberSTR, out int numberInt))
+        {
+            if (numberInt <= 0) Console.WriteLine("Введите число больше нуля");
+            else
+            {
+                number = numberInt;
+                isInputInt = false;
+            }
+        }
+        else
+            Console.WriteLine("Ввели не число");
+    }
+    return number;
+}
+
+int[,] InitMatrix(int firstDemension, int secondDemension)   // Создание и наполнение матрицы
+{
+    int[,] matrix = new int[firstDemension, secondDemension];
+    Random rnd = new Random();
+    for (int i = 0; i < firstDemension; i++)
+    {
+        for (int j = 0; j < secondDemension; j++)
+            matrix[i, j] = rnd.Next(1, 20);
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)    // Вывод матрицы в консоль
+{
+    Console.WriteLine("Заполненная матрица");
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+
+        Console.WriteLine();
+    }
+}
+
+void Calculation(int[,] matrix)
+{
+    int sum;
+    Console.Write($"Среднее арифмитическое столбцов ->");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.Write($"  {(double)sum / matrix.GetLength(0),3:N1}");
+    }
+}
+int firstDemension = NumberInput("Введите кол-во строк:  ");
+int secondDemension = NumberInput("Введите столбцов:  ");
+
+int[,] resultMatrix = InitMatrix(firstDemension, secondDemension);
+PrintMatrix(resultMatrix);
+
+Calculation(resultMatrix);
+
+Console.WriteLine();
